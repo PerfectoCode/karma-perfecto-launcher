e818964d8626c77c7867a8542ee8a3bbb95e9c3d
morrishoresh <morrish@perfectomobile.com>
2019-04-29 19:10:23 +0300

    add README and sample configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Perfecto Reporting SDK for NodeJS projects
+*The* solution for digital automation projects using NodeJS
+
+# Usage
+Sample configuration can be found in karma.conf.js
---
+++ karma.conf.js
@@ -0,0 +1,86 @@
+// sample configuration file
+
+module.exports = function(config) {
+
+const customLaunchers = {
+
+  // We can run multiple browsers
+
+  chrome72: {
+    base: 'Perfecto',
+    capabilities: {
+	  platformName : 'Windows',
+      platformVersion : '10',
+      browserName : 'Chrome',
+      browserVersion : '72',
+      resolution : '1280x1024',
+      location : 'US East'
+    }
+  },
+
+  chrome71: {
+    base: 'Perfecto',
+    capabilities: {
+	  platformName : 'Windows',
+      platformVersion : '10',
+      browserName : 'Chrome',
+      browserVersion : '71',
+      resolution : '1280x1024',
+      location : 'US East'
+    }
+  },
+};
+
+  config.set({
+
+    // It may take some time to initialize a connection
+    captureTimeout: 120000,
+
+    perfecto: {
+		// Set if there is already a running tunnel.
+		// The environment variable PERFECTO_TUNNEL_ID can be set instead.
+		// If this parameter is not set, the launcher will start a new tunnel
+		// If the value is 'none' then no tunnel is used.
+		// OPTIONAL
+		tunnelId: '',
+
+		// The local hostname or host address.
+		// If not set this value is determined automatically..
+		// OPTIONAL
+		host: '',
+
+		// The full path of perfectoconnect binary.
+		// If not set it is assumed that the binary is in the executable path
+		// OPTIONAL
+		perfectoConnect: '',
+
+		// The Perfecto Cloud URL
+		// MANDATORY
+		perfectoUrl: 's',
+
+		// The security token
+		// MANDATORY
+		securityToken: '',
+
+		// the name of the test for the report
+		// OPTIONAL
+		testName: '',
+
+		// Place holder for capablities that apply for all browser
+		// OPTIONAL
+		capabilities: {
+		}
+
+    },
+
+    browsers: ['Perfecto'],
+
+    // This is the preferred method of execution.
+    singleRun: true,
+
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+
+    reporters: ['Perfecto']
+  })
+}
